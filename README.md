6e4668bf9a8ef73960c6c9a9b9d97ae29a054d77
RobsonMaker <rb9344062@gmail.com>
2024-03-09 18:21:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Script de configuração pós instalação \n Para executar abra o terminal(bash) no diretório onde está o arquivo .sh e digite ./<nome_do_arquivo.sh> \n ou digite: sh <nome_do_arquivo.sh> 
---
+++ script_pos_instalacao_ubuntu.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Atualizar lista de pacotes
+sudo apt update
+
+# Instalar MariaDB
+sudo apt install mariadb-server
+
+# Instalar MySQL Workbench
+sudo apt install mysql-workbench
+
+# Instalar Visual Studio Code
+sudo snap install --classic code
+
+# Instalar NetBeans
+sudo snap install netbeans --classic
+
+# Instalar Java JDK
+sudo apt install default-jdk
+
+# Instalar GRASS GIS
+sudo apt install grass
+
+# Instalar PyCharm Professional
+sudo snap install pycharm-professional --classic
+
+# Instalar Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# Instalar Microsoft Edge
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" | sudo tee /etc/apt/sources.list.d/microsoft-edge-dev.list
+sudo rm microsoft.gpg
+sudo apt update
+sudo apt install microsoft-edge-dev
+
+# Instalar GParted
+sudo apt install gparted
+
+# Instalar VIM
+sudo apt install vim
+
+# Instalar Git
+sudo apt install git
+
+# Instalar Gnome Software
+sudo apt install gnome-software gnome-software-plugin-flatpak gnome-software-plugin-snap
+
+# Instalar KDE Connect
+sudo apt install kdeconnect
+
